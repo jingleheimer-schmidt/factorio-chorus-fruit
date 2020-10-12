1b80e50909e7e803e01ce7f82a1a9758ed093b1a
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-12 13:47:16 -0700

    major cleanup. added functions. attempts to teleport 8x before failing

--- control.lua
+++ control.lua
@@ -1,88 +1,107 @@
+
+-- find a non-colliding position within ±64 tiles to teleport to
+function find_valid_position(player, player_position)
+  -- calculate offsets
+  local x_offset = math.random(-64,64)
+  local y_offset = math.random(-64,64)
+  -- calculate new position
+  local new_position = {
+    x = (player_position.x + x_offset),
+    y = (player_position.y + y_offset)
+  }
+  -- check if new player can fit in new position
+  local valid_position = player.surface.find_non_colliding_position("character", new_position, 2, 1)
+  return valid_position
+end
+
+-- calculate ouch factor
+function calculate_ouch(player, player_position, valid_position)
+  -- calculates distance between player and valid_position
+  local distance = math.floor(((player_position.x - valid_position.x) ^ 2 + (player_position.y - valid_position.y) ^ 2) ^ 0.5)
+  -- calculates a random variable between .91 and 1.09
+  local almost = (math.random(91, 109))*.01
+  -- sets ouch as ~88% of distance traveled from player health
+  local ouch = distance*(.88*almost)
+  return ouch
+end
+
+function run_damage_logic(valid_position, player, ouch)
+  -- if player health will be at or below zero then kill them
+  if ( ouch <= 0 ) then
+    game.play_sound{
+      path = "fall-big", position = valid_position, volume_modifier = 1}
+      player.die()
+  -- otherwise subtract ~88% of distance traveled from player health
+  else
+    player.health = player.health - ouch
+    if ( ouch <= 16) then
+      game.play_sound{
+        path = "fall-small", position = valid_position, volume_modifier = 1}
+    else
+      game.play_sound{
+        path = "fall-big", position = valid_position, volume_modifier = 1}
+    end
+  end
+end
+
+function chorus_fruit_teleport(player, player_position, valid_position)
+  -- calculate ouch
+  local ouch = calculate_ouch(player, player_position, valid_position)
+  -- teleports player to valid_position
+  player.teleport(valid_position)
+  -- damage or kill player, play sounds
+  run_damage_logic(valid_position, player, ouch)
+end
+
 -- runs when a script is triggered
 script.on_event(defines.events.on_script_trigger_effect, function(event)
-  -- checks if capsule is chorus fruit, then tries twice to teleport the player randomly within ±64 tiles
+  -- checks if capsule is chorus fruit
   if event.effect_id == "chorus_fruit_effect_id" then
-    -- calculate offsets
-    local x_offset_1 = math.random(-64,64)
-    local y_offset_1 = math.random(-64,64)
     -- adds the player and player position as a variable
     local player = event.source_entity
     local player_position = event.source_position
-    -- calculate potential teleport location
-    local new_position_1 = {
-      x = (player_position.x + x_offset_1),
-      y = (player_position.y + y_offset_1)
-    }
-    -- finds a non-colliding position within 10 tiles of new_position
-    local valid_position_1 = player.surface.find_non_colliding_position("character", new_position_1, 10, 1)
-    if valid_position_1 then
-      -- calculates distance between player and valid_position
-      local distance = math.floor(((player_position.x - valid_position_1.x) ^ 2 + (player_position.y - valid_position_1.y) ^ 2) ^ 0.5)
-      -- calculates a random variable between .91 and 1.09
-      local almost = (math.random(91, 109))*.01
-      -- saves health deduction
-      local health_deduction = distance*(.88*almost)
-      -- subtracts ~88% of distance traveled from player health
-      local ouch = player.health - health_deduction
-      -- teleports player to valid_position
-      player.teleport(valid_position_1)
-      -- if player health will be at or below zero then kill them
-		  if ( ouch <= 0 ) then
-           game.play_sound{
-             path = "fall-big", position = valid_position_1, volume_modifier = 1}
-           player.die()
-      -- otherwise subtract ~88% of distance traveled from player health
-      else
-			   player.health = ouch
-         if ( health_deduction <= 16) then
-           game.play_sound{
-             path = "fall-small", position = valid_position_1, volume_modifier = 1}
-         else
-           game.play_sound{
-             path = "fall-big", position = valid_position_1, volume_modifier = 1}
-         end
-		  end
+    -- attempt to teleport the player randomly within ±64 tiles 8 times 
+    local valid_position = find_valid_position(player, player_position)
+    if valid_position then
+      chorus_fruit_teleport(player, player_position, valid_position)
     else
-      -- try to teleport again: calculate new offsets
-      local x_offset_2 = math.random(-64,64)
-      local y_offset_2 = math.random(-64,64)
-      local new_position_2 = {
-        x = (player_position.x + x_offset_2),
-        y = (player_position.y + y_offset_2)
-      }
-      -- find a non-colliding position within 10 tiles of new_position_2
-      local valid_position_2 = player.surface.find_non_colliding_position("character", new_position_2, 10, 1)
-      if valid_position_2 then
-        -- calculates distance between player and valid_position_2
-        local distance = math.floor(((player_position.x - valid_position_2.x) ^ 2 + (player_position.y - valid_position_2.y) ^ 2) ^ 0.5)
-        -- calculates a random variable between .91 and 1.09
-        local almost = (math.random(91, 109))*.01
-        -- saves health deduction
-        local health_deduction = distance*(.88*almost)
-        -- subtracts ~88% of distance traveled from player health
-        local ouch = player.health - health_deduction
-        -- teleports player to secondary valid_position
-        player.teleport(valid_position_2)
-        -- if player health will be at or below zero then kill them
-  		  if ( ouch <= 0 ) then
-             game.play_sound{
-               path = "fall-big", position = valid_position_2, volume_modifier = 1}
-             player.die()
-        -- otherwise subtract ~88% of distance traveled from player health
-        else
-  			   player.health = ouch
-           if ( health_deduction <= 16) then
-             game.play_sound{
-               path = "fall-small", position = valid_position_2, volume_modifier = 1}
-           else
-             game.play_sound{
-               path = "fall-big", position = valid_position_2, volume_modifier = 1}
-           end
-  		  end
+      local valid_position = find_valid_position(player, player_position)
+      if valid_position then
+        chorus_fruit_teleport(player, player_position, valid_position)
       else
-        -- player.player.print("No safe landing nearby")
-        game.play_sound{
-        path = "ender-pearl-failed-landing", position = player_position, volume_modifier = 1}
+        local valid_position = find_valid_position(player, player_position)
+        if valid_position then
+          chorus_fruit_teleport(player, player_position, valid_position)
+        else
+          local valid_position = find_valid_position(player, player_position)
+          if valid_position then
+            chorus_fruit_teleport(player, player_position, valid_position)
+          else
+            local valid_position = find_valid_position(player, player_position)
+            if valid_position then
+              chorus_fruit_teleport(player, player_position, valid_position)
+            else
+              local valid_position = find_valid_position(player, player_position)
+              if valid_position then
+                chorus_fruit_teleport(player, player_position, valid_position)
+              else
+                local valid_position = find_valid_position(player, player_position)
+                if valid_position then
+                  chorus_fruit_teleport(player, player_position, valid_position)
+                else
+                  local valid_position = find_valid_position(player, player_position)
+                  if valid_position then
+                    chorus_fruit_teleport(player, player_position, valid_position)
+                  else
+                    -- player.player.print("No safe landing nearby")
+                    game.play_sound{
+                    path = "ender-pearl-failed-landing", position = player_position, volume_modifier = 1}
+                  end
+                end
+              end
+            end
+          end
+        end
       end
     end
   else
