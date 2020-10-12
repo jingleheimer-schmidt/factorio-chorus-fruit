e9757b6728c228614966d352b4443ca7ebdf4c55
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-12 11:56:36 -0700

    added some sounds

---
+++ damage-sounds.lua
@@ -0,0 +1,18 @@
+local fallSmall = {
+  name = "fall-small",
+  type = "sound",
+  filename = "__factorio-chorus-fruit__/sounds/fallsmall.ogg",
+  volume = 1
+}
+
+local fallBig = {
+  name = "fall-big",
+  type = "sound",
+  filename = "__factorio-chorus-fruit__/sounds/fallbig.ogg",
+  volume = 1
+}
+
+data:extend({
+  fallSmall,
+  fallBig
+})
---
+++ eating-sound.lua
@@ -0,0 +1,19 @@
+local sounds = {}
+
+sounds.eat_chorus_fruit_sound =
+{
+  {
+    filename = "__factorio-chorus-fruit__/sounds/eat1.ogg",
+    volume = 0.6
+  },
+  {
+    filename = "__factorio-chorus-fruit__/sounds/eat2.ogg",
+    volume = 0.6
+  },
+  {
+    filename = "__factorio-chorus-fruit__/sounds/eat3.ogg",
+    volume = 0.6
+  }
+}
+
+return sounds
