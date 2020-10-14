6b28d5be7391e0a778c23755a399d28316224f55
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-14 16:07:33 -0700

    made eating sound a little bit louder

--- eating-sounds.lua
+++ eating-sounds.lua
@@ -4,15 +4,15 @@ sounds.eat_chorus_fruit_sound =
 {
   {
     filename = "__factorio-chorus-fruit__/sounds/eat1.ogg",
-    volume = 0.6
+    volume = 0.7
   },
   {
     filename = "__factorio-chorus-fruit__/sounds/eat2.ogg",
-    volume = 0.6
+    volume = 0.7
   },
   {
     filename = "__factorio-chorus-fruit__/sounds/eat3.ogg",
-    volume = 0.6
+    volume = 0.7
   }
 }
 
