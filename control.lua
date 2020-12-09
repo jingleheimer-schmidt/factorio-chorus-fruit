6b05eabe64adae4d6700f6e5e8eb1d174d7d349a
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-12-08 23:48:11 -0800

    removed some old code, simplified teleport logic

--- control.lua
+++ control.lua
@@ -24,25 +24,15 @@ end
 
 -- deal damage or kill player, and play damage and teleporting sounds
 function run_damage_logic(valid_position, player, ouch)
-  -- if player health will be at or below zero then kill them
   if ( ouch <= 0 ) then
     game.play_sound{
       path = "fall-big", position = valid_position, volume_modifier = 1}
-      -- player.die()
-  -- otherwise subtract ~88% of distance traveled from player health
   else
-    -- player.health = player.health - ouch
     if ( ouch <= 8) then
-      -- game.play_sound{
-      --   path = "fall-small", position = valid_position, volume_modifier = .7
-      -- }
       game.play_sound{
         path = "portal-1", position = valid_position, volume_modifier = .7
       }
     else
-      -- game.play_sound{
-      --   path = "fall-big", position = valid_position, volume_modifier = .7
-      -- }
       game.play_sound{
         path = "portal-2", position = valid_position, volume_modifier = .7
       }
@@ -56,61 +46,29 @@ function chorus_fruit_teleport(player, player_position, valid_position)
   local ouch = calculate_ouch(player, player_position, valid_position)
   -- teleports player to valid_position
   player.teleport(valid_position)
-  -- damage or kill player, play sounds
+  -- play sounds
   run_damage_logic(valid_position, player, ouch)
+end
+
+function find_position_and_teleport(event)
+  -- adds the player and player position as a variable
+  local player = event.source_entity
+  local player_position = event.source_position
+  -- attempt to teleport the player randomly within ±64 tiles 16 times
+  for i=1,16 do
+    local valid_position = find_valid_position(player, player_position)
+    if valid_position then
+      chorus_fruit_teleport(player, player_position, valid_position)
+      break
+    end
+  end
 end
 
 -- runs when a script is triggered
 script.on_event(defines.events.on_script_trigger_effect, function(event)
   -- checks if capsule is chorus fruit
   if event.effect_id == "chorus_fruit_effect_id" then
-    -- adds the player and player position as a variable
-    local player = event.source_entity
-    local player_position = event.source_position
-    -- attempt to teleport the player randomly within ±32 tiles 8 times
-    local valid_position = find_valid_position(player, player_position)
-    if valid_position then
-      chorus_fruit_teleport(player, player_position, valid_position)
-    else
-      local valid_position = find_valid_position(player, player_position)
-      if valid_position then
-        chorus_fruit_teleport(player, player_position, valid_position)
-      else
-        local valid_position = find_valid_position(player, player_position)
-        if valid_position then
-          chorus_fruit_teleport(player, player_position, valid_position)
-        else
-          local valid_position = find_valid_position(player, player_position)
-          if valid_position then
-            chorus_fruit_teleport(player, player_position, valid_position)
-          else
-            local valid_position = find_valid_position(player, player_position)
-            if valid_position then
-              chorus_fruit_teleport(player, player_position, valid_position)
-            else
-              local valid_position = find_valid_position(player, player_position)
-              if valid_position then
-                chorus_fruit_teleport(player, player_position, valid_position)
-              else
-                local valid_position = find_valid_position(player, player_position)
-                if valid_position then
-                  chorus_fruit_teleport(player, player_position, valid_position)
-                else
-                  local valid_position = find_valid_position(player, player_position)
-                  if valid_position then
-                    chorus_fruit_teleport(player, player_position, valid_position)
-                  else
-                    -- -- player.player.print("No safe landing nearby")
-                    -- game.play_sound{
-                    -- path = "ender-pearl-failed-landing", position = player_position, volume_modifier = 1}
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
-    end
+    find_position_and_teleport(event)
   else
     return
   end
